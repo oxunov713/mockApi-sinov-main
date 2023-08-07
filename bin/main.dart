b271d54e77cef807a8c11a007401275e1561955d
oxunov_713 <aoxunov713@gmail.com>
2023-08-07 09:58:05 +0500

    put ni qo'shdim

--- bin/main.dart
+++ bin/main.dart
@@ -7,10 +7,10 @@ import '../lib/models/todo_model.dart';
 Future<void> main() async {
   String baseUrl = "https://64c9fb25b2980cec85c2ab7a.mockapi.io";
 
-  Uri uriForTodos = Uri.parse("$baseUrl/todos");
-  Uri uriForUsers = Uri.parse("$baseUrl/Users");
+  // Uri uriForTodos = Uri.parse("$baseUrl/todos");
+  // Uri uriForUsers = Uri.parse("$baseUrl/Users");
   /* 
- ///This is [get] 
+ //!This is [get] 
   myHTTP.Response responseGetTodos = await myHTTP.get(uriForTodos);
   myHTTP.Response responseGetUsers = await myHTTP.get(uriForUsers);
 
@@ -26,16 +26,17 @@ Future<void> main() async {
   print(todos[0].account_name);
   print(users[0].married);
 */
-  ///This is [post]
+  /*
+  //!This is [post]
 
-  TodoModel todo1 = TodoModel(
+   TodoModel todo1 = TodoModel(
       country: 'Posttodo', city: 'Posttodo', account_name: 'Posttodo');
   //tododa yangi obyekt yasab oldim
 
   TodoModel todo2 = TodoModel(country: '000000000', city: '00000');
 //obyekt oldim
   TodoModel todo3 = TodoModel();
- 
+
   myHTTP.Response responsePostTodos1 = await myHTTP.post(
     uriForTodos,
     headers: <String, String>{},
@@ -56,4 +57,16 @@ Future<void> main() async {
         String>{}, //agar header yozilmasa,bo'sh bo'lsa o'zi yangi malumot bn to'ldiardi agar headers yozilgan bolsa malumoti bo'lmasa null bn to'ldiradi
     body: jsonEncode(todo3.toJson()),
   ); //post qildim
+*/
+/*
+  //!This is [put]
+  Uri uri = await Uri.parse("$baseUrl/todos/6"); //6chi id dagi mapni opkeladi
+  myHTTP.Response responsePut = await myHTTP.put(
+    uri,
+    headers: <String, String>{
+      "Content-Type": "application/json; charset=UTF-8"
+    },
+    body: jsonEncode({'country': 'Parij'}), // country sini Parijga o'zgartiradi
+  );
+  */
 }
